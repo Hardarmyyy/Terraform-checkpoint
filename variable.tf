0ed8ea832c9e63f02d2217b84160692cfe08c192
hardarmyyy <adeyemoadams005@gmail.com>
2023-11-19 19:18:12 +0100

    add variable values

--- variable.tf
+++ variable.tf
@@ -1,3 +1,9 @@
-variable "ami_name" {}
-variable "ami_id" {}
-variable "ami_key_pair_name" {}
+variable "ami_name" {
+    default = "FlaskAppInstance"
+}
+variable "ami_id" {
+    default = "ami-0fc5d935ebf8bc3bc"
+}
+variable "ami_key_pair_name" {
+    default = "tera"
+}
